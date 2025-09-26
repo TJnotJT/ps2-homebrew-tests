4317a8af18a0b35695287c01b5c1da2f46827443
TJ <none@none.com>
2025-09-26 10:42:44 -0400

    Add texturing to line test 7.

--- line-test-7/line-test-7.c
+++ line-test-7/line-test-7.c
@@ -39,6 +39,10 @@
 #define USE_AA 0
 #endif
 
+#ifndef USE_TEX
+#define USE_TEX 0
+#endif
+
 #define FRAME_WIDTH 128
 #define FRAME_HEIGHT 256
 #define WINDOW_X (2048 - FRAME_WIDTH / 2)
@@ -48,11 +52,35 @@
 #define TEST_REGIONS_Y 64
 #define TEST_REGIONS (TEST_REGIONS_X * TEST_REGIONS_Y)
 
+#define TEX_BUF_WIDTH 256
+#define TEX_WIDTH 256
+#define TEX_HEIGHT 256
+
 framebuffer_t g_frame; // Frame buffer
 zbuffer_t g_z; // Z buffer
+texbuffer_t g_tex; // Texture buffer
+clutbuffer_t g_clut;
+lod_t g_lod;
+texwrap_t g_wrap;
 
 u8 g_frame_data[FRAME_WIDTH * FRAME_HEIGHT * 4] __attribute__((aligned(64)));
 u8 g_z_data[FRAME_WIDTH * FRAME_HEIGHT * 4] __attribute__((aligned(64)));
+u8 g_texture_data[256 * 256 * 4] __attribute__((aligned(64)));
+
+void make_texture()
+{
+	for (int y = 0; y < TEX_HEIGHT; y++)
+	{
+		for (int x = 0; x < TEX_WIDTH; x++)
+		{
+			int i = (y * TEX_WIDTH + x) * 4;
+			g_texture_data[i + 0] = x;
+			g_texture_data[i + 1] = y;
+			g_texture_data[i + 2] = 0;
+			g_texture_data[i + 3] = 0xFF;
+		}
+	}
+}
 
 int graph_initialize_custom()
 {
@@ -85,7 +113,70 @@ void init_gs()
 	g_z.zsm = GS_ZBUF_32;
 	g_z.address = graph_vram_allocate(FRAME_WIDTH, FRAME_HEIGHT, GS_PSMZ_32, GRAPH_ALIGN_PAGE);
 
+	g_tex.width = TEX_BUF_WIDTH;
+	g_tex.psm = GS_PSM_32;
+	g_tex.address = graph_vram_allocate(TEX_BUF_WIDTH, TEX_HEIGHT, GS_PSM_32, GRAPH_ALIGN_BLOCK);
+
+	g_tex.info.width = draw_log2(TEX_WIDTH);
+	g_tex.info.height = draw_log2(TEX_HEIGHT);
+	g_tex.info.function = TEXTURE_FUNCTION_DECAL;
+	g_tex.info.components = TEXTURE_COMPONENTS_RGBA;
+
+	g_clut.storage_mode = CLUT_STORAGE_MODE1;
+	g_clut.start = 0;
+	g_clut.psm = 0;
+	g_clut.load_method = CLUT_NO_LOAD;
+	g_clut.address = 0;
+
+	g_lod.calculation = LOD_USE_K;
+	g_lod.max_level = 0;
+	g_lod.mag_filter = LOD_MAG_NEAREST;
+	g_lod.min_filter = LOD_MIN_NEAREST;
+	g_lod.l = 0;
+	g_lod.k = 0;
+
+	g_clut.storage_mode = CLUT_STORAGE_MODE1;
+	g_clut.start = 0;
+	g_clut.psm = 0;
+	g_clut.load_method = CLUT_NO_LOAD;
+	g_clut.address = 0;
+
+	g_wrap.horizontal = WRAP_REPEAT;
+	g_wrap.vertical = WRAP_REPEAT;
+	g_wrap.minu = 0;
+	g_wrap.maxu = 0 ;
+	g_wrap.minv = 0;
+	g_wrap.maxv = 0;
+
+
 	graph_initialize_custom();
+}
+
+qword_t *setup_texture(qword_t* q)
+{
+	q = draw_texture_sampling(q, 0, &g_lod);
+	q = draw_texturebuffer(q, 0, &g_tex, &g_clut);
+	q = draw_texture_wrapping(q, 0, &g_wrap);
+
+	return q;
+}
+
+void load_texture(texbuffer_t *texbuf)
+{
+	qword_t *packet = aligned_alloc(64, sizeof(qword_t) * 1024);
+
+	qword_t *q = packet;
+
+	q = draw_texture_transfer(q, g_texture_data, 1 << texbuf->info.width, 1 << texbuf->info.height,
+		texbuf->psm, texbuf->address, texbuf->width);
+	q = draw_texture_flush(q);
+
+	FlushCache(0); // Needed because we generate data in the EE.
+
+	dma_channel_send_chain(DMA_CHANNEL_GIF, packet, q - packet, 0, 0);
+	dma_channel_wait(DMA_CHANNEL_GIF, -1);
+
+	free(packet);
 }
 
 qword_t *my_draw_disable_tests(qword_t *q, int context)
@@ -148,6 +239,10 @@ qword_t* my_draw_line(qword_t* q, int region)
 	int c0 = cx * ((region / 512) % 2);
 	int c1 = cx - c0;
 
+	int U0 = 16 * c0 + 8;
+	int V0 = 8;
+	int U1 = 16 * c1 + 8;
+	int V1 = 8;
 	int X0 = (x + 1) * 16 + fx0;
 	int Y0 = (y + 1) * 16 + fy0;
 	int X1 = (x + 1) * 16 + fx0 + 16;
@@ -160,19 +255,27 @@ qword_t* my_draw_line(qword_t* q, int region)
 		int tmp;
 		tmp = X0; X0 = Y0; Y0 = tmp;
 		tmp = X1; X1 = Y1; Y1 = tmp;
+		tmp = U0; U0 = V0; V0 = tmp;
+		tmp = U1; U1 = V1; V1 = tmp;
 	}
 
-  PACK_GIFTAG(q, GIF_SET_TAG(5, 0, 0, 0, GIF_FLG_PACKED, 1), GIF_REG_AD);
+  PACK_GIFTAG(q, GIF_SET_TAG(7, 0, 0, 0, GIF_FLG_PACKED, 1), GIF_REG_AD);
   q++;
 
-  PACK_GIFTAG(q, GS_SET_PRIM(GS_PRIM_LINE, 1, 0, 0, 0, USE_AA, 0, 0, 0), GIF_REG_PRIM);
+  PACK_GIFTAG(q, GS_SET_PRIM(GS_PRIM_LINE, 1, USE_TEX, 0, 0, USE_AA, 1, 0, 0), GIF_REG_PRIM);
   q++;
+
+	PACK_GIFTAG(q, GS_SET_UV(U0, V0), GS_REG_UV);
+	q++;
 	
   PACK_GIFTAG(q, 0xFF000000 | c0, GS_REG_RGBAQ);
   q++;
 
   PACK_GIFTAG(q, GS_SET_XYZ(X0, Y0, Z0), GS_REG_XYZ2);
   q++;
+
+	PACK_GIFTAG(q, GS_SET_UV(U1, V1), GS_REG_UV);
+	q++;
 
 	PACK_GIFTAG(q, 0xFF000000 | c1, GS_REG_RGBAQ);
   q++;
@@ -200,6 +303,7 @@ int render_test()
   // Set up the drawing environment and clear the frame buffer
 	q = packet;
 	q = draw_setup_environment(q, 0, &g_frame, &g_z);
+	q = setup_texture(q);
 	q = draw_primitive_xyoffset(q, 0, WINDOW_X, WINDOW_Y);
 	q = my_draw_clear(q, 0);
 	q = my_draw_disable_tests(q, 0);
@@ -237,6 +341,10 @@ int main(int argc, char *argv[])
 	
   init_gs();
 
+	make_texture();
+
+	load_texture(&g_tex);
+
 	render_test();
 
 	read_framebuffer(g_frame.address, FRAME_WIDTH / 64, 0, 0, FRAME_WIDTH, FRAME_HEIGHT, g_frame.psm, g_frame_data);
@@ -244,7 +352,7 @@ int main(int argc, char *argv[])
 	read_framebuffer(g_z.address, FRAME_WIDTH / 64, 0, 0, FRAME_WIDTH, FRAME_HEIGHT, GS_PSMZ_32, g_z_data);
 
 	char filename[64];
-	sprintf(filename, "mass:line_test_7%s%s.bmp", VERTICAL ? "_v" : "_h", USE_AA ? "_aa" : "");
+	sprintf(filename, "mass:line_test_7%s%s%s.bmp", VERTICAL ? "_v" : "_h", USE_AA ? "_aa" : "", USE_TEX ? "_tex" : "");
 
 	if (write_bmp_to_usb(filename, g_frame_data, FRAME_WIDTH, FRAME_HEIGHT, g_frame.psm, my_draw_clear_send) != 0)
 		printf("Failed to write line test data to USB\n");
