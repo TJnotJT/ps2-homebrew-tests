6068c2fc7b61f6f0a18876ce4226caac1cf52b41
TJ <none@none.com>
2025-12-12 17:55:45 -0500

    Add sample bilinear test.

--- sample-bilinear/sample-bilinear.c
+++ sample-bilinear/sample-bilinear.c
@@ -25,67 +25,38 @@
 #include <draw.h>
 #include <draw3d.h>
 #include <math3d.h>
-#include <math.h>
 
 #include "../lib-common/common.h"
 #include "../lib-read-fb/read-fb.h"
 #include "../lib-bmp/bmp.h"
 #include "../lib-usb/usb.h"
 
-#ifndef VARY_ALPHA
-#define VARY_ALPHA 0
-#endif
+#define TYPE_COLOR 0
+#define TYPE_DEPTH 1
+
+#define FILTER_NEAREST 0
+#define FILTER_LINEAR 1
 
 #define FRAME_WIDTH 512
-#define FRAME_HEIGHT 512
+#define FRAME_HEIGHT 256
 #define WINDOW_X (2048 - FRAME_WIDTH / 2)
 #define WINDOW_Y (2048 - FRAME_HEIGHT / 2)
-#define TEST_REGION_SIZE 64
-#define TEST_REGIONS_X 4
-#define TEST_REGIONS_Y 4
-#define TEST_REGIONS 16
 
-#define TEX_BUF_WIDTH 256
-#define TEX_WIDTH 256
-#define TEX_HEIGHT 256
+#define TEX_BUF_WIDTH 64
+#define TEX_WIDTH 64
+#define TEX_HEIGHT 32
 
 framebuffer_t g_frame; // Frame buffer
+framebuffer_t g_frame_src; // Frame buffer for source
 zbuffer_t g_z; // Z buffer
-texbuffer_t g_tex; // Texture buffer
+zbuffer_t g_z_src; // Z buffer for source
+texbuffer_t g_tex_color; // Texture buffer
+texbuffer_t g_tex_depth; // Texture buffer
 clutbuffer_t g_clut;
 lod_t g_lod;
 texwrap_t g_wrap;
 
 u8 g_frame_data[FRAME_WIDTH * FRAME_HEIGHT * 4] __attribute__((aligned(64)));
-u8 g_z_data[FRAME_WIDTH * FRAME_HEIGHT * 4] __attribute__((aligned(64)));
-u8 g_texture_data[TEX_WIDTH * TEX_HEIGHT * 4] __attribute__((aligned(64)));
-
-int max(int a, int b)
-{
-	return (a > b) ? a : b;
-}
-
-int min(int a, int b)
-{
-	return (a < b) ? a : b;
-}
-
-void make_texture()
-{
-	for (int y = 0; y < TEX_HEIGHT; y++)
-	{
-		for (int x = 0; x < TEX_WIDTH; x++)
-		{
-			int i = (y * TEX_WIDTH + x) * 4;
-			g_texture_data[i + 0] = x;
-			g_texture_data[i + 1] = y;
-			g_texture_data[i + 2] = 0;
-			g_texture_data[i + 3] = 0xFF;
-		}
-	}
-
-	FlushCache(0); // Needed because we generate data in the EE.
-}
 
 int graph_initialize_custom()
 {
@@ -110,7 +81,7 @@ void init_gs()
 	g_frame.height = FRAME_HEIGHT;
 	g_frame.mask = 0;
 	g_frame.psm = GS_PSM_32;
-	g_frame.address = graph_vram_allocate(FRAME_WIDTH, FRAME_HEIGHT, g_frame.psm, GRAPH_ALIGN_PAGE);
+	g_frame.address = graph_vram_allocate(FRAME_WIDTH, FRAME_HEIGHT, GS_PSM_32, GRAPH_ALIGN_PAGE);
 
 	g_z.enable = DRAW_ENABLE;
 	g_z.mask = 0;
@@ -118,14 +89,35 @@ void init_gs()
 	g_z.zsm = GS_ZBUF_32;
 	g_z.address = graph_vram_allocate(FRAME_WIDTH, FRAME_HEIGHT, GS_PSMZ_32, GRAPH_ALIGN_PAGE);
 
-	g_tex.width = TEX_BUF_WIDTH;
-	g_tex.psm = GS_PSM_32;
-	g_tex.address = graph_vram_allocate(TEX_BUF_WIDTH, TEX_HEIGHT, GS_PSM_32, GRAPH_ALIGN_BLOCK);
+	g_frame_src.width = TEX_BUF_WIDTH;
+	g_frame_src.height = TEX_HEIGHT;
+	g_frame_src.mask = 0;
+	g_frame_src.psm = GS_PSM_32;
+	g_frame_src.address = graph_vram_allocate(TEX_BUF_WIDTH, TEX_HEIGHT, GS_PSM_32, GRAPH_ALIGN_PAGE);
 
-	g_tex.info.width = draw_log2(TEX_WIDTH);
-	g_tex.info.height = draw_log2(TEX_HEIGHT);
-	g_tex.info.function = TEXTURE_FUNCTION_DECAL;
-	g_tex.info.components = TEXTURE_COMPONENTS_RGBA;
+	g_z_src.enable = DRAW_ENABLE;
+	g_z_src.mask = 0;
+	g_z_src.method = ZTEST_METHOD_ALLPASS;	
+	g_z_src.zsm = GS_ZBUF_32;
+	g_z_src.address = graph_vram_allocate(TEX_BUF_WIDTH, TEX_HEIGHT, GS_PSMZ_32, GRAPH_ALIGN_PAGE);
+
+	g_tex_color.width = TEX_BUF_WIDTH;
+	g_tex_color.psm = GS_PSM_32;
+	g_tex_color.address = g_frame_src.address;
+
+	g_tex_color.info.width = draw_log2(TEX_WIDTH);
+	g_tex_color.info.height = draw_log2(TEX_HEIGHT);
+	g_tex_color.info.function = TEXTURE_FUNCTION_DECAL;
+	g_tex_color.info.components = TEXTURE_COMPONENTS_RGBA;
+
+	g_tex_depth.width = TEX_BUF_WIDTH;
+	g_tex_depth.psm = GS_PSMZ_32;
+	g_tex_depth.address = g_z_src.address;
+
+	g_tex_depth.info.width = draw_log2(TEX_WIDTH);
+	g_tex_depth.info.height = draw_log2(TEX_HEIGHT);
+	g_tex_depth.info.function = TEXTURE_FUNCTION_DECAL;
+	g_tex_depth.info.components = TEXTURE_COMPONENTS_RGBA;
 
 	g_clut.storage_mode = CLUT_STORAGE_MODE1;
 	g_clut.start = 0;
@@ -136,7 +128,7 @@ void init_gs()
 	g_lod.calculation = LOD_USE_K;
 	g_lod.max_level = 0;
 	g_lod.mag_filter = LOD_MAG_NEAREST;
-	g_lod.min_filter = LOD_MIN_NEAREST;
+	g_lod.min_filter = LOD_MAG_NEAREST;
 	g_lod.l = 0;
 	g_lod.k = 0;
 
@@ -156,34 +148,18 @@ void init_gs()
 	graph_initialize_custom();
 }
 
-qword_t *setup_texture(qword_t* q)
+qword_t* setup_texture(qword_t* q, int type, int filter)
 {
-	q = draw_texture_sampling(q, 0, &g_lod);
-	q = draw_texturebuffer(q, 0, &g_tex, &g_clut);
+	lod_t lod = g_lod;
+	lod.mag_filter = filter == FILTER_LINEAR ? LOD_MAG_LINEAR : LOD_MAG_NEAREST;
+	lod.min_filter = filter == FILTER_LINEAR ? LOD_MIN_LINEAR : LOD_MIN_NEAREST;
+	q = draw_texture_sampling(q, 0, &lod);
+	q = draw_texturebuffer(q, 0, type == TYPE_DEPTH ? &g_tex_depth : &g_tex_color, &g_clut);
 	q = draw_texture_wrapping(q, 0, &g_wrap);
-
 	return q;
 }
 
-void load_texture(texbuffer_t *texbuf)
-{
-	qword_t *packet = aligned_alloc(64, sizeof(qword_t) * 1024);
-
-	qword_t *q = packet;
-
-	q = draw_texture_transfer(q, g_texture_data, 1 << texbuf->info.width, 1 << texbuf->info.height,
-		texbuf->psm, texbuf->address, texbuf->width);
-	q = draw_texture_flush(q);
-
-	FlushCache(0); // Needed because we generate data in the EE.
-
-	dma_channel_send_chain(DMA_CHANNEL_GIF, packet, q - packet, 0, 0);
-	dma_channel_wait(DMA_CHANNEL_GIF, -1);
-
-	free(packet);
-}
-
-qword_t *my_draw_disable_tests(qword_t *q, int context)
+qword_t* my_draw_disable_tests(qword_t *q, int context)
 {
 	PACK_GIFTAG(q, GIF_SET_TAG(1, 0, 0, 0, GIF_FLG_PACKED, 1), GIF_REG_AD);
 	q++;
@@ -192,7 +168,6 @@ qword_t *my_draw_disable_tests(qword_t *q, int context)
 							   DRAW_ENABLE, ZTEST_METHOD_ALLPASS), GS_REG_TEST + context);
 	q++;
 	return q;
-
 }
 
 qword_t* my_draw_clear(qword_t* q, unsigned rgb)
@@ -231,85 +206,79 @@ void my_draw_clear_send(unsigned rgb)
 	free(packet);
 }
 
-qword_t* my_draw_line(qword_t* q, int region)
+int send_dummy_eop()
 {
-	unsigned use_aa = (region & 1) == 1;
-	unsigned use_abe = (region & 2) == 2;
-	unsigned alpha_index = (region >> 2) & 3;
-	int alpha_values[4] = { 64, 128, 192, 255 };
-	int vary_amt = VARY_ALPHA ? 2 : 0;
-	unsigned alpha0 = (unsigned)max(alpha_values[alpha_index] - vary_amt, 0);
-	unsigned alpha1 = (unsigned)min(alpha_values[alpha_index] + vary_amt, 255);
-	unsigned color0 = (alpha0 << 24) | 0x404040;
-	unsigned color1 = (alpha1 << 24) | 0x404040;
+	qword_t* packet = aligned_alloc(64, sizeof(qword_t) * 32);
+	qword_t* q = packet;
 
-	const int x = WINDOW_X + TEST_REGION_SIZE * (region % TEST_REGIONS_X);
-	const int y = WINDOW_Y + TEST_REGION_SIZE * (region / TEST_REGIONS_X);
+	// Send a dummy EOP packet
+	q = draw_finish(q);
+	dma_channel_send_normal(DMA_CHANNEL_GIF, packet, q - packet, 0, 0);
+	dma_channel_wait(DMA_CHANNEL_GIF, 0);
+	draw_wait_finish();
 
-	int fx0 = 16;
-	int fy0 = 16 * (TEST_REGION_SIZE / 2) + 4;
-	int fx1 = 16 * (TEST_REGION_SIZE - 2);
-	int fy1 = 16 * (TEST_REGION_SIZE / 2) + 4;
+	free(packet);
+	return 0;
+}
 
-	int U0 = rand() % (TEX_WIDTH * 16);
-	int V0 = rand() % (TEX_HEIGHT * 16);
-	int U1 = rand() % (TEX_WIDTH * 16);
-	int V1 = rand() % (TEX_HEIGHT * 16);
-	int X0 = (x + 1) * 16 + fx0;
-	int Y0 = (y + 1) * 16 + fy0;
-	int X1 = (x + 1) * 16 + fx1;
-	int Y1 = (y + 1) * 16 + fy1;
-	int Z0 = 0;
-	int Z1 = 1;
+int render_src()
+{
+	qword_t* packet = aligned_alloc(64, sizeof(qword_t) * 8192);
+	qword_t* q;
 
-  PACK_GIFTAG(q, GIF_SET_TAG(8, 0, 0, 0, GIF_FLG_PACKED, 1), GIF_REG_AD);
-  q++;
+	// Set up the drawing environment and clear the frame buffer
+	q = packet;
+	q = draw_setup_environment(q, 0, &g_frame_src, &g_z_src);
+	q = draw_primitive_xyoffset(q, 0, 0, 0);
+	q = my_draw_clear(q, 0);
+	q = my_draw_disable_tests(q, 0);
+	q = draw_finish(q);
+	dma_channel_send_normal(DMA_CHANNEL_GIF, packet, q - packet, 0, 0);
+	dma_channel_wait(DMA_CHANNEL_GIF, 0);
+	draw_wait_finish();
 
-  PACK_GIFTAG(q, GS_SET_PRIM(GS_PRIM_LINE, 1, 0, 0, use_abe, use_aa, 1, 0, 0), GIF_REG_PRIM);
-  q++;
+	// Draw a checkerboard pattern
+	for (int y = 0; y < TEX_HEIGHT; y += 1)
+	{
+		q = packet;
+		
+		PACK_GIFTAG(q, GIF_SET_TAG(TEX_WIDTH * 3, 0, 0, GS_PRIM_POINT, GIF_FLG_PACKED, 1), GIF_REG_AD);
+		q++;
+		for (int x = 0; x < TEX_WIDTH; x += 1)
+		{
+			unsigned color = ((x + y) % 2) ? 0xFFFFFFFF : 0xFF000000;
 
-	PACK_GIFTAG(q, GS_SET_ALPHA(0, 2, 0, 0, 64), GS_REG_ALPHA);
-	q++;
+			PACK_GIFTAG(q, GIF_SET_PRIM(PRIM_POINT, 0, 0, 0, 0, 0, 0, 0, 0), GIF_REG_PRIM);
+			q++;
+			PACK_GIFTAG(q, color, GIF_REG_RGBAQ);
+			q++;
+			PACK_GIFTAG(q, GIF_SET_XYZ(x * 16, y * 16, color), GIF_REG_XYZ2);
+			q++;
+		}
+		q = draw_finish(q);
+		dma_channel_send_normal(DMA_CHANNEL_GIF, packet, q - packet, 0, 0);
+		dma_channel_wait(DMA_CHANNEL_GIF, 0);
+		draw_wait_finish();
+	}
 
-	PACK_GIFTAG(q, GS_SET_UV(U0, V0), GS_REG_UV);
-	q++;
-	
-  PACK_GIFTAG(q, color0, GS_REG_RGBAQ);
-  q++;
-
-  PACK_GIFTAG(q, GS_SET_XYZ(X0, Y0, Z0), GS_REG_XYZ2);
-  q++;
-
-	PACK_GIFTAG(q, GS_SET_UV(U1, V1), GS_REG_UV);
-	q++;
-
-	PACK_GIFTAG(q, color1, GS_REG_RGBAQ);
-  q++;
-
-  PACK_GIFTAG(q, GS_SET_XYZ(X1, Y1, Z1), GS_REG_XYZ2);
-  q++;
-
-	return q;
+	free(packet);
+	return 0;
 }
 
 int render_test()
 {
 	srand(123);
 
+	send_dummy_eop();
+
+	render_src();
+
 	qword_t* packet = aligned_alloc(64, sizeof(qword_t) * 8192);
 	qword_t* q;
-
-	// Send a dummy EOP packet
-	q = packet;
-	q = draw_finish(q);
-	dma_channel_send_normal(DMA_CHANNEL_GIF, packet, q - packet, 0, 0);
-	dma_channel_wait(DMA_CHANNEL_GIF, 0);
-	draw_wait_finish();
 
   // Set up the drawing environment and clear the frame buffer
 	q = packet;
 	q = draw_setup_environment(q, 0, &g_frame, &g_z);
-	q = setup_texture(q);
 	q = draw_primitive_xyoffset(q, 0, WINDOW_X, WINDOW_Y);
 	q = my_draw_clear(q, 0);
 	q = my_draw_disable_tests(q, 0);
@@ -318,10 +287,37 @@ int render_test()
 	dma_channel_wait(DMA_CHANNEL_GIF, 0);
 	draw_wait_finish();
 
-	q = packet;
-	for (int region = 0; region < TEST_REGIONS; region++)
+	// Draw a sprite magnifying the texture
+	int y_offset = WINDOW_Y;
+	for (int type = TYPE_COLOR; type <= TYPE_DEPTH; type++)
 	{
-		q = my_draw_line(q, region);
+		int x_offset = WINDOW_X;
+		for (int filter = FILTER_NEAREST; filter <= FILTER_LINEAR; filter++)
+		{
+			q = packet;
+			q = setup_texture(q, type, filter);
+			PACK_GIFTAG(q, GIF_SET_TAG(7, 0, 0, 0, GIF_FLG_PACKED, 1), GIF_REG_AD);
+			q++;
+			PACK_GIFTAG(q, GIF_SET_PRIM(PRIM_SPRITE, 0, 1, 0, 0, 0, 1, 0, 0), GIF_REG_PRIM);
+			q++;
+			PACK_GIFTAG(q, GIF_SET_UV(0, 0), GIF_REG_UV);
+			q++;
+			PACK_GIFTAG(q, 0x80808080, GIF_REG_RGBAQ);
+			q++;
+			PACK_GIFTAG(q, GIF_SET_XYZ(x_offset << 4, y_offset << 4, 0), GIF_REG_XYZ2);
+			q++;
+			PACK_GIFTAG(q, GIF_SET_UV((TEX_WIDTH - 2) << 4, (TEX_HEIGHT - 2) << 4), GIF_REG_UV);
+			q++;
+			PACK_GIFTAG(q, 0x80808080, GIF_REG_RGBAQ);
+			q++;
+			PACK_GIFTAG(q, GIF_SET_XYZ((x_offset + FRAME_WIDTH / 2 - 4) << 4, (y_offset + FRAME_HEIGHT / 2 - 4) << 4, 0), GIF_REG_XYZ2);
+			q++;
+			dma_channel_send_normal(DMA_CHANNEL_GIF, packet, q - packet, 0, 0);
+			dma_channel_wait(DMA_CHANNEL_GIF, 0);
+
+			x_offset += FRAME_WIDTH / 2;
+		}
+		y_offset += FRAME_HEIGHT / 2;
 	}
 
 	q = draw_finish(q);
@@ -340,23 +336,17 @@ int main(int argc, char *argv[])
 	
   init_gs();
 
-	make_texture();
-
-	load_texture(&g_tex);
-
 	render_test();
 
 	read_framebuffer(g_frame.address, FRAME_WIDTH / 64, 0, 0, FRAME_WIDTH, FRAME_HEIGHT, g_frame.psm, g_frame_data);
 
-	read_framebuffer(g_z.address, FRAME_WIDTH / 64, 0, 0, FRAME_WIDTH, FRAME_HEIGHT, GS_PSMZ_32, g_z_data);
-
 	char filename[64];
-	sprintf(filename, "mass:line_test_9%s.bmp", VARY_ALPHA ? "_varyalpha" : "");
+	sprintf(filename, "mass:sample_bilinear.bmp");
 
 	if (write_bmp_to_usb(filename, g_frame_data, FRAME_WIDTH, FRAME_HEIGHT, g_frame.psm, my_draw_clear_send) != 0)
-		printf("Failed to write line test data to USB\n");
+		printf("Failed to write point test data to USB\n");
 	else
-		printf("Wrote line test data to USB\n");
+		printf("Wrote point test data to USB\n");
 
 	SleepThread();
 
